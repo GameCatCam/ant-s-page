19278655f51349e1ebccfad40684b3f05a6471be
Anthony Iacano <iacano5@outlook.com>
2023-11-21 18:42:46 -0500

    commiting local commits from last night

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,18 @@
 export default function Nav({ links }) {
     return (
         <>
-            
-        </>
+            {/* <!--Start of the CONTACT:SOCIAL MEDIA Sub-Section--> */}
+            <ui class="social-media-group">
+                <li><a href="https://www.instagram.com/"><img class = "social-media-icons" src = "../src/assets/images/instagram-social.png" alt="an icon representing Instagram"/></a></li>
+                <li><a href="https://www.twitter.com/"><img class = "social-media-icons" src = "../src/assets/images/twitter-social.png" alt="an icon representing Twitter"/></a></li>
+                <li><a href="https://www.youtube.com/"><img class = "social-media-icons" src = "../src/assets/images/youtube-social.png" alt="an icon representing YouTube"/></a></li>
+                <li><a href="https://www.linkedin.com/in/anthony-iacano/"><img class = "social-media-icons" src = "../src/assets/images/linkedin-social.png" alt="an icon representing Linkedin"/></a></li>
+                <li><a href="https://github.com/Anthony-GG/"><img class = "social-media-icons" src = "../src/assets/images/github-social.png" alt="an icon representing GitHub"/></a></li>
+            </ui>
 
+            <footer>
+                <p> Anthony Iacano 2023</p>
+            </footer>
+        </>
     );
   }
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,6 +2,13 @@ import Header from "../components/Header";
 
 export default function AboutMe(){
     return(
-        <div><p>Contact Me</p></div>
+        <>
+            {/* <!--Start of the CONTACT Section--> */}
+            <section class = "contact" id="contact">
+                <h2>CONTACT ME</h2>
+                <p>Whether you want to talk to me about your favorite movie, or about an exciting idea/project you might have, feel free to reach out using the button below!</p>
+                <button type="button" id="button"><a href="mailto:anthonymiacanojr@gmail.com">CLICK HERE</a></button>
+            </section>
+        </>
     );
 }
