1793bfce91aa78f61793795dacca39a341c5ff33
Anthony Iacano <aiacano@overdrive.com>
2023-11-21 16:00:56 -0500

    added basic logic for resume page, will need styling

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,5 +1,8 @@
 export default function Resume(){
     return(
-        <div><p>Resume</p></div>
+        <>
+        <a href="..\src\assets\documents\Anthony_Iacano_Resume.pdf" download="..\src\assets\assets\documents\Anthony_Iacano_Resume.pdf" type="button" class="btn btn-outline-primary"> Download Resume </a>
+        <img id="Resume" src="..\src\assets\documents\Anthony_Iacano_Resume.png" alt="Anthony's Resume" class="card-image-top mt-3 mw-100"></img>
+        </>
     );
 }
