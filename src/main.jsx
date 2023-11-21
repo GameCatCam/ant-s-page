e30b0a2bfd48cbf47687835e6403dcfb90da8bc6
Anthony Iacano <iacano5@outlook.com>
2023-11-20 22:02:46 -0500

    added route info and empty page/component files

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,47 @@
+// Brought in the required imports from 'react-router-dom' to set up application routing behavior
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+//imports pages for the router to route
+import AboutMe from './pages/AboutMe.jsx'
+import Portfolio from './pages/Portfolio.jsx'
+import Contact from './pages/Contact.jsx'
+import Resume from './pages/Resume.jsx'
+import Error from './pages/Error.jsx'
+
+//Sets up router and provides route for the URL
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <AboutMe />,
+      },
+      {
+        path: 'portfolio',
+        element: <Portfolio />,
+      },
+      {
+        path: 'contact',
+        element: <Contact />,
+      },
+      {
+        path: 'resume',
+        element: <Resume />,
+      },
+    ],
+  },
+])
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  //provides router to render on page
+    <RouterProvider router={router} />
 )
